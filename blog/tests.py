bbee30af3a4a4da3e80034af84a70324a72a722c
DaeWon98 <ghduf16@naver.com>
2024-04-04 10:30:58 +0900

    작성자 정보 출력

--- blog/tests.py
+++ blog/tests.py
@@ -1,11 +1,15 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
-
+from django.contrib.auth.models import User
 
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_camry = User.objects.create_user(username='camry',
+                                                   password='korea2002')
+        self.user_toyota = User.objects.create_user(username='toyota',
+                                                   password='korea2002')
 
     def navbar_test(self, soup) :
         navbar = soup.nav
@@ -42,11 +46,13 @@ class TestView(TestCase):
         post_001 = Post.objects.create(
             title='첫번째 포스트입니다.',
             content='Hello World. We are the world.',
+            author = self.user_camry
         )
 
         post_002 = Post.objects.create(
             title='두번째 포스트입니다.',
             content='1등이 전부는 아니잖아요?',
+            author = self.user_toyota
         )
 
         self.assertEqual(Post.objects.count(), 2)
@@ -61,11 +67,15 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
+        self.assertIn(self.user_camry.username.upper(), main_area.text)
+        self.assertIn(self.user_toyota.username.upper(), main_area.text)
+
     def test_post_detail(self):
         # 0.   Post가 하나 있다.
         post_001 = Post.objects.create(
             title='첫번째 포스트입니다.',
             content='Hello World. We are the world.',
+            author = self.user_camry
         )
         # 0.1  그 포스트의 url은 'blog/1/' 이다.
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
@@ -87,7 +97,7 @@ class TestView(TestCase):
         self.assertIn(post_001.title, post_area.text)
 
         # 1.5  첫 번째 post의 작성자(author)가 post-area에 있다.
-        # 아직 작성 불가
+        self.assertIn(self.user_camry.username.upper(), post_area.text)
 
         # 1.6  첫 번째 post의 content가 post-area에 있다.
         self.assertIn(post_001.content, post_area.text)
